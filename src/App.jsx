665551266527e5bfe3965f1af4ef596716af7167
andrea-zecevic <azecev01@gmail.com>
2024-03-11 20:21:09 +0100

    Added 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Navigation from "./components/Navigation";
 import { ProductsPage } from "./pages/ProductsPage";
 import { ProductPage } from "./pages/ProductPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
 function App() {
   return (
@@ -13,8 +19,10 @@ function App() {
       <div>
         <Navigation />
         <Routes>
+          <Route path="/" element={<Navigate replace to="/products" />} /> {}
           <Route path="/products" element={<ProductsPage />} />
           <Route path="/product/:productId" element={<ProductPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
     </Router>
